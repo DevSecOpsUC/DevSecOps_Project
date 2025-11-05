e3721e88071c65108711e63f02741289d5da3c2f
AndresBarrios18 <pipe292011@gmail.com>
2025-11-05 11:59:34 -0500

    Mejora Sonar Cloud

---
+++ Unidad1_AsegurandoCodigoFuente/auth-service.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using System.Net.Http.Json;
+
+
+namespace AuthService.Tests;
+
+public class AuthServiceTests : IClassFixture<WebApplicationFactory<AuthService.Program>>
+{
+    private readonly WebApplicationFactory<AuthService.Program> _factory;
+
+    public AuthServiceTests(WebApplicationFactory<AuthService.Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task LoginEndpoint_Returns_Unauthorized_For_InvalidCredentials()
+    {
+        var client = _factory.CreateClient();
+        var response = await client.PostAsJsonAsync("/api/auth/login", new { Username = "bad", Password = "wrong" });
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+}
---
+++ Unidad1_AsegurandoCodigoFuente/rooms-service.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace RoomsService.Tests;
+
+public class RoomsServiceTests : IClassFixture<WebApplicationFactory<RoomsService.Program>>
+{
+    private readonly WebApplicationFactory<RoomsService.Program> _factory;
+
+    public RoomsServiceTests(WebApplicationFactory<RoomsService.Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task RoomsEndpoint_Returns_Success()
+    {
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync("/api/rooms");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
